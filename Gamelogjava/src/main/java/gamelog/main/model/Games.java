9cd168834b379428f96848bbe1b87d40d532be67
SeraphimThomas <foxboyseraphim@gmail.com>
2021-10-06 08:02:05 -0500

    new stuff

--- Gamelogjava/src/main/java/gamelog/main/model/Games.java
+++ Gamelogjava/src/main/java/gamelog/main/model/Games.java
@@ -1,5 +1,89 @@
 package gamelog.main.model;
 
+import java.util.Date;
+
 public class Games {
+	private int userid;
+	private String gamename;
+	private String gamesystem;
+	private double playtime;
+	private Date dateadded;
+	private Date datechanged;
+
+	public Games() {
+		super();
+	}
+
+	public Games(int userid) {
+		super();
+		this.userid = userid;
+	}
+
+	public Games(int userid, String gamename, String gamesystem, double playtime, Date datechanged) {
+		super();
+		this.userid = userid;
+		this.gamename = gamename;
+		this.gamesystem = gamesystem;
+		this.playtime = playtime;
+		this.datechanged = datechanged;
+	}
+
+	public Games(int userid, String gamename, String gamesystem, double playtime, Date dateadded, Date datechanged) {
+		super();
+		this.userid = userid;
+		this.gamename = gamename;
+		this.gamesystem = gamesystem;
+		this.playtime = playtime;
+		this.dateadded = dateadded;
+		this.datechanged = datechanged;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getGamename() {
+		return gamename;
+	}
+
+	public void setGamename(String gamename) {
+		this.gamename = gamename;
+	}
+
+	public String getGamesystem() {
+		return gamesystem;
+	}
+
+	public void setGamesystem(String gamesystem) {
+		this.gamesystem = gamesystem;
+	}
+
+	public double getPlaytime() {
+		return playtime;
+	}
+
+	public void setPlaytime(double playtime) {
+		this.playtime = playtime;
+	}
+
+	public Date getDateadded() {
+		return dateadded;
+	}
+
+	public void setDateadded(Date dateadded) {
+		this.dateadded = dateadded;
+	}
+
+	public Date getDatechanged() {
+		return datechanged;
+	}
+
+	public void setDatechanged(Date datechanged) {
+		this.datechanged = datechanged;
+	}
 
 }
--- Gamelogjava/src/main/java/gamelog/main/model/User.java
+++ Gamelogjava/src/main/java/gamelog/main/model/User.java
@@ -1,5 +1,72 @@
 package gamelog.main.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int userid;
+
+	@OneToMany(mappedBy = "userid", cascade = CascadeType.ALL)
+	private List<Games> games;
+
+	@Column
+	String username;
+	@Column
+	String password;
+
+	public User() {
+		super();
+	}
+
+	public User(int userid, List<Games> games, String username, String password) {
+		super();
+		this.userid = userid;
+		this.games = games;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public List<Games> getGames() {
+		return games;
+	}
+
+	public void setGames(List<Games> games) {
+		this.games = games;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
