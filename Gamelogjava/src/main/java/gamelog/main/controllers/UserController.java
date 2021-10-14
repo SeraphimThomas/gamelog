5f610e99f5e185c567365aaef07d1c2b546da199
SeraphimThomas <foxboyseraphim@gmail.com>
2021-10-14 10:34:23 -0500

    Altered security
    Took out spring security till I learn it better.  Using SHA-1 encryption for passwords now to save them on the back end securely

--- Gamelogjava/src/main/java/gamelog/main/controllers/UserController.java
+++ Gamelogjava/src/main/java/gamelog/main/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gamelog.main.encryption.Encryption;
 import gamelog.main.model.User;
 import gamelog.main.services.UserManager;
 
@@ -19,18 +20,16 @@ public class UserController {
 	@Autowired
 	private UserManager userMan;
 	
-//	@Autowired
-//	private PasswordEncoder encoder;
-	
 	@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping(path="/enter", consumes="application/json", produces="application/json")
 	public ResponseEntity<User> login(@RequestBody User user){
+		user.setPassword(Encryption.getHashSHA1(user.getPassword()));
 		return new ResponseEntity<>(userMan.login(user),HttpStatus.CREATED);
 	}
 	
 	@PostMapping(path="/create",consumes="application/json", produces="application/json")
 	public ResponseEntity<User> createAccount(@RequestBody User user){
-//		user.setPassword(encoder.encode(user.getPassword()));
+		user.setPassword(Encryption.getHashSHA1(user.getPassword()));
 		return new ResponseEntity<>(userMan.createAccount(user), HttpStatus.CREATED);
 	}
 }
